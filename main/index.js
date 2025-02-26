08f7e54a1739a945be69ef22b0b8f266d6d64ff6
Kooming <eekrud3805@gmail.com>
2025-02-27 01:31:41 +0900

    main 페이지 글 등록 까지

--- main/index.js
+++ main/index.js
@@ -2,8 +2,32 @@ const  user = {
     nickname : "ming"
 }
 
+
 const nickname = document.querySelector(".nickname")
 
 nickname.innerHTML = `${user.nickname}님 환영합니다.`
 
-localStorage.getItem("posts", JSON.stringify)
+const posts = JSON.parse(localStorage.getItem("posts")) || [] ;
+
+const bodyContent = document.querySelector(".body-content");
+
+const drawing = () => {
+    bodyContent.innerHTML = ""
+
+    posts.forEach((post, index) => {
+        console.log(`Post ${index}:`, post);
+
+        const postItem = document.createElement("ul");
+        postItem.classList.add("border-item");
+        postItem.innerHTML = `
+                <li>${index + 1}</li>
+                <li>${post.title}</li>
+                <li>${post.text}</li>
+                <li>${post.date}</li>
+                `;  
+                bodyContent.append(postItem);
+            })
+    }
+        
+drawing();
+
